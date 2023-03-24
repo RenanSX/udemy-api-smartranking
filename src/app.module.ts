7502613cf12481eb36dc186edc78823a6cd6ffb1
Renan Silva Xavier <renansxdev@gmail.com>
2023-03-24 09:48:37 -0300

    adicionado novos desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { JogadoresModule } from "./jogadores/jogadores.module";
 import { CategoriasModule } from './categorias/categorias.module';
 import { DesafiosModule } from './desafios/desafios.module';
+import { AwsModule } from './aws/aws.module';
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://renansx:1299102869@cluster0.t9ezojr.mongodb.net/?retryWrites=true&w=majority"),
     JogadoresModule,
     CategoriasModule,
     DesafiosModule,
+    AwsModule,
   ],
   controllers: [],
   providers: [],
